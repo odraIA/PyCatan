bc16cbb15cc50f0ec2f12deb82bd02c911d2f5f1
Ricardo Díaz <144832810+odraIA@users.noreply.github.com>
2026-04-25 13:51:49 +0200

    ultimo job ya lo juro

--- benchmark_random.py
+++ benchmark_random.py
@@ -12,7 +12,7 @@ porcentaje_workers = 0.95
 
 # Agentes a evaluar: (ruta_clase, params)
 agentes_a_evaluar = [
-    ("Agents.AdrianHerasAgent.AdrianHerasAgent", None), # Por ejemplo, si quieres evaluar el agente AdrianHerasAgent, que está en Agents.AdrianHerasAgent sin parámetros adicionales
+    ("Agents.HeuristicAgent.HeuristicAgent", None), # Por ejemplo, si quieres evaluar el agente AdrianHerasAgent, que está en Agents.AdrianHerasAgent sin parámetros adicionales
     # Se pueden poner varios agentes para evaluar y comparar, con y sin parámetros personalizados, por si queremos probar varias configuraciones del mismo agente.
 ]
 
